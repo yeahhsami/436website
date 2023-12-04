ef27cc899a80850ad3d26bb47722e2828c8a792f
yeahhsami <134347051+yeahhsami@users.noreply.github.com>
2023-12-03 21:50:59 -0500

    Add files via upload

---
+++ home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+
+def main(): 
+    st.title("UniCentral")
+    st.write("Welcome to Unicentral!")
+    st.write("Ready to stay on track!")
+    st.caption("Upload your syllabus to the syllabus reader tool and get access to all of your classes information!")
+    
+    
+     
+
+
+
+if __name__ == '__main__':
+    main()  
